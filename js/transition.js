f675ac2e404cdf88757da0e17c55b1ced0f6cf55
eramirez04 <emerssonramirez510@gmail.com>
2023-06-16 12:55:26 -0500

    transition witdh buttom toggle

--- js/transition.js
+++ js/transition.js
@@ -1,13 +1,17 @@
 let boton =  document.getElementById("btn");
-let btn = document.getElementById("toggle");
+let toggle = document.getElementById("toggle");
 
 boton.addEventListener("click", function(){
-    if (btn.classList.contains("box-4-1")){
-        btn.classList.add("box-4-1-2");
-        btn.classList.remove("box-4-1"); 
+    if (toggle.classList.contains("box-4-1")){
+        toggle.classList.add("box-4-1-2");
+        toggle.classList.remove("box-4-1"); 
+        boton.classList.add("box-4-4")
+        boton.classList.remove("box-4")
     }
     else{
-        btn.classList.remove("box-4-1-2");
-        btn.classList.add("box-4-1");
+        toggle.classList.remove("box-4-1-2");
+        toggle.classList.add("box-4-1");
+        boton.classList.remove("box-4-4")
+        boton.classList.add("box-4")
     }
 });
