5e4ed1bfbc5872e258c3f9c3419c6891de08e559
eramirez04 <emerssonramirez510@gmail.com>
2023-06-16 17:33:35 -0500

    new media query to index

---
+++ js/index.js
@@ -0,0 +1,15 @@
+/* let logoHambuerguer =document.getElementById("logoHamburguer");
+let menu = document.getElementsByTagName("foto");
+
+
+logoHambuerguer.addEventListener("click", function(){
+    if(menu.classList.contains("box1-1")){
+        menu.classList.add("box1-1-2")
+        menu.classList.remove("box1-1")
+    }
+    else{
+        menu.classList.remove("box1-1-2")
+        menu.classList.remove("box1-1")
+    }
+
+}) */
