72f40ee2c81c2eea228a47adde479ca6ff04b6e4
eramirez04 <emerssonramirez510@gmail.com>
2023-08-12 20:50:41 -0500

    css grid y maquetacion

--- js/transition.js
+++ js/transition.js
@@ -1,14 +1,14 @@
-let boton =  document.getElementById("btn");
+let boton = document.getElementById("btn");
 let toggle = document.getElementById("toggle");
 
-boton.addEventListener("click", function(){
-    if (toggle.classList.contains("box-4-1")){
+boton.addEventListener("click", function () {
+    if (toggle.classList.contains("box-4-1")) {
         toggle.classList.add("box-4-1-2");
-        toggle.classList.remove("box-4-1"); 
+        toggle.classList.remove("box-4-1");
         boton.classList.add("box-4-4")
         boton.classList.remove("box-4")
     }
-    else{
+    else {
         toggle.classList.remove("box-4-1-2");
         toggle.classList.add("box-4-1");
         boton.classList.remove("box-4-4")
