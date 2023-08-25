7b8dbdc1a10c102d7d828bcc57a510a60c5c07a6
eramirez04 <emerssonramirez510@gmail.com>
2023-08-25 14:50:04 -0500

    starbucks

--- js/starbukcs.js
+++ js/starbukcs.js
@@ -1,3 +1,29 @@
-function cambiarImg(anything) {
+/* function cambiarImg(anything) {
     document.querySelector('.star').src = anything;
-}
+} */
+
+let iceGreen = document.getElementById("iceGreen");
+let iceRose  = document.getElementById("iceRose");
+let mainCircle  = document.getElementById("mainCircle")
+let mainImg = document.getElementById("mainImg")
+
+iceGreen.addEventListener("mouseover", function(){
+    mainCircle.classList.add("main-circle-green")
+    mainCircle.classList.remove("main-circle-rose")
+    mainCircle.classList.remove("main-circle-pink")
+    mainImg.setAttribute("src", "img/img1.png")
+})
+iceRose.addEventListener("mouseover", function(){
+    mainCircle.classList.remove("main-circle-green")
+    mainCircle.classList.add("main-circle-rose")
+    mainCircle.classList.remove("main-circle-pink")
+    mainImg.setAttribute("src", "img/img2.png")
+
+})
+icePink.addEventListener("mouseover", function(){
+    mainCircle.classList.remove("main-circle-green")
+    mainCircle.classList.remove("main-circle-rose")
+    mainCircle.classList.add("main-circle-pink")
+    mainImg.setAttribute("src", "img/img3.png")
+
+})  
